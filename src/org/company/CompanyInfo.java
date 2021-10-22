22827d72ec697a3fc8da33667ef8f1b9ddd26ca7
MohanaKrishnanT <mohanakrishnan7755@gmail.com>
2021-10-22 15:57:39 -0700

    mohanjj

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,23 @@
+package org.company;
+
+public class CompanyInfo {
+public void companyName() {
+	System.out.println("greens");
+	
+}
+private void companyIde() {
+	System.out.println("approved");
+
+}
+private void companyAddress() {
+	System.out.println("t.nagar");
+}
+public static void main(String[] args) {
+	CompanyInfo call=new CompanyInfo();
+	call.companyName();
+	call.companyIde();
+	call.companyAddress();
+}
+
+}
+
